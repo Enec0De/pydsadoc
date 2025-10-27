d473d31c6e9fdca87cbe1126925f1dbd8be29bb8
Aina <75526775+Enec0De@users.noreply.github.com>
2025-10-27 23:12:35 +0800

    finish huffman tree

--- Demo.py/main.py
+++ Demo.py/main.py
@@ -4,46 +4,11 @@
 from __future__ import annotations
 from typing import Optional
 
+a = []
 
-class Node:
-    
-    def __init__(self, data: Optional[int]):
+try:
+    a[4] = 1
+except IndexError:
+    a.append(4)
 
-        self.data = data
-        self.next: Optional[Node]= None
-
-
-class LinkedStack:
-
-    def __init__(self):
-
-        self.head = None
-
-    def push(self, element: int):
-
-        s = Node(element)
-
-        s.next = self.head
-        self.head = s
-
-
-    def pop(self):
-
-        if self.head is not None:
-            value = self.head.data
-            self.head = self.head.next
-
-        else:
-            raise IndexError
-
-        return value
-
-    def __str__(self):
-
-        s: str = ''
-
-        while self.head is not None:
-           s = s + ' ' + str(self.head.data) 
-           self.head = self.head.next
-
-        return s
+print('a is: ', a)
