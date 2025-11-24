dcb2a5bcb5eb79790ee56c39ce6970568177dd19
Aina <75526775+Enec0De@users.noreply.github.com>
2025-11-24 14:16:03 +0800

    finish insertion_sort

--- src/sort/insertion.py
+++ src/sort/insertion.py
@@ -6,6 +6,7 @@ __all__ = ['insertion_sort']
 __version__ = '0.1'
 __author__ = 'Aina'
 
+import random
 from typing import Union, Optional
 
 # Define the ElementType
@@ -14,5 +15,31 @@ ElementType = Union[int]
 
 def insertion_sort(arr: list[ElementType]) -> None:
     """The Selection Sort Algorithm."""
-    
-    ...
+    length = len(arr)
+    for i in range(1, length):
+        temp = arr[i]
+        j = 1
+        # Insert the element into the list that has been sorted.
+        while i - j >= 0 and arr[i-j] > temp:
+            arr[i-j+1] = arr[i-j]
+            j += 1
+        else:
+            arr[i-j+1] = temp
+
+
+def main() -> None:
+    # Create random list.
+    arr = [random.randint(-9,99) for _ in range(random.randint(1,10))]
+    print(arr)
+
+    # Sort array.
+    insertion_sort(arr)
+    print(arr)
+
+    # Assertion test.
+    for i in range(len(arr)-1):
+        assert arr[i] <= arr[i+1]
+
+
+if __name__ == '__main__':
+    main()
